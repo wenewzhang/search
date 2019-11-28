ef7dbce5a2b921f27e7686efed50bb9b3a723765
wenewzhang <wenewboy@gmail.com>
2019-11-28 13:23:52 +0800

    sustain args instead of clone

--- poem/src/main.rs
+++ poem/src/main.rs
@@ -5,21 +5,27 @@ use search::run;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args, false);
+    let config = match Config::new(env::args(), false) {
+        Ok(c) => c,
+        Err(e ) => {
+            println!("Application error: {}", e);
+            process::exit(1)
+        },
+    };
     // run(config);
     if let Err(e) = search::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
     println!("End of insensitive search!");
-    let config2 = Config::new(&args, true);
-    // config2.sensitive = true;
-    if let Err(e) = search::run(config2) {
-        println!("Application error: {}", e);
-        process::exit(1);
-    }
+    // let config2 = Config::new(&args, true);
+    // // config2.sensitive = true;
+    // if let Err(e) = search::run(config2) {
+    //     println!("Application error: {}", e);
+    //     process::exit(1);
+    // }
     // --snip--
 }
 
--- search/src/lib.rs
+++ search/src/lib.rs
@@ -55,15 +55,9 @@ fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
@@ -84,10 +78,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 impl Config {
-    pub fn new(args: &[String], sensitive: bool) -> Config {
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args, sensitive: bool) ->  Result<Config, &'static str> {
+        args.next();
 
-        Config { query, filename, sensitive }
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        Ok(Config { query, filename, sensitive })
     }
 }
